2c4d55b11189db27be5bcdb4e1c7bd7425c91bdc
Gustavo Silva <silva95gustavo@gmail.com>
2016-05-09 12:30:53 +0100

    ...

--- Server/www/api/api.php
+++ Server/www/api/api.php
@@ -1,4 +1,19 @@
 <?php
-$requestParts = explode('/',$_GET['request']);
-print_r($requestParts);
+header('Content-Type: application/json; charset=utf-8');
+
+$requestParts = explode('/', rtrim($_GET['request'], '/'));
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($requestParts[0]) {
+	case "events": {
+		
+	}
+	break;
+	case "users": {
+		
+	}
+	break;
+}
+
+echo json_encode($requestParts);
 ?>
