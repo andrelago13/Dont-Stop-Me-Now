d84d54e7a0e3811ec50997b034949ba194dacd29
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-21 15:40:29 +0100

    Submit event ready for API integration

--- Client/DSMN-Client/dsmn/src/main/java/com/sdis/g0102/dsmn/CreateEventActivity.java
+++ Client/DSMN-Client/dsmn/src/main/java/com/sdis/g0102/dsmn/CreateEventActivity.java
@@ -1,56 +1,33 @@
 package com.sdis.g0102.dsmn;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.io.IOException;
 
-public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, GoogleApiClient.OnConnectionFailedListener {
+public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final String select_image_text = "Select image";
     private static final String clear_image_text = "Clear image";
@@ -58,10 +35,18 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
     public static final int SELECT_IMAGE = 4132;
     public static final int PLACE_PICKER_REQUEST = 1112;
 
+    private Event.Type event_type = null;
+    private Place event_location = null;
+    private String event_description = null;
+    private Bitmap event_selected_image = null;
+
     private ImageView event_icon;
     private ImageView event_image;
-    private Bitmap selected_image = null;
     private Button select_picture_button;
+    private LinearLayout no_location_layout;
+    private LinearLayout good_location_layout;
+    private TextView location_text;
+    private TextView description_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,37 +57,16 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
         event_image = (ImageView) findViewById(R.id.event_image);
         select_picture_button = (Button) findViewById(R.id.select_image_button);
         select_picture_button.setText(select_image_text);
+        no_location_layout = (LinearLayout) findViewById(R.id.no_location_layout);
+        good_location_layout = (LinearLayout) findViewById(R.id.good_location_layout);
+        location_text = (TextView) findViewById(R.id.location_name);
+        description_text = (TextView) findViewById(R.id.create_event_description);
 
         Spinner spinner = (Spinner) findViewById(R.id.choose_type_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.event_types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-        GoogleApiClient mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, this)
-                .build();
-
-        //createLocationRequest();
-    }
-
-    protected void createLocationRequest() {
-
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
-        try {
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException e) {
-            Log.d("CreateEventActivity", "PlacePicker exception 1");
-            // TODO toast
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            Log.d("CreateEventActivity", "PlacePicker exception 2");
-            // TODO toast
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -111,12 +75,16 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
         Log.d("CreateEventActivity", "Selected: " + selected);
 
         if(selected.equals("Vehicle Crash")) {
+            event_type = Event.Type.CRASH;
             event_icon.setImageResource(R.drawable.event_crash);
         } else if(selected.equals("Speed Radar")) {
+            event_type = Event.Type.RADAR;
             event_icon.setImageResource(R.drawable.event_camera);
         } else if(selected.equals("Slow Traffic")) {
+            event_type = Event.Type.TRAFFIC;
             event_icon.setImageResource(R.drawable.event_traffic);
         } else if(selected.equals("Traffic Stop")) {
+            event_type = Event.Type.STOP;
             event_icon.setImageResource(R.drawable.event_stop);
         }
     }
@@ -127,21 +95,95 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
     }
 
     public void choosePicture(View v) {
-        if(selected_image == null) {
+        if(event_selected_image == null) {
             Intent intent = new Intent();
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
         } else {
-            selected_image = null;
+            event_selected_image = null;
             event_image.setImageDrawable(null);
             select_picture_button.setText(select_image_text);
         }
     }
 
     public void submit(View v) {
+
+        event_description = description_text.getText().toString();
+
+        if(event_description == null || event_description.equals("")) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Event description cannot be empty.")
+                    .setCancelable(false)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {}
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+            return;
+        }
+
+        if(event_location == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("You must select a location for the event.")
+                    .setCancelable(false)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {}
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+            return;
+        }
+
+        if(event_selected_image == null) {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Submitting Event")
+                    .setMessage("Are you sure you want to submit the event without a picture?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            actualSubmit();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+            return;
+        }
+
         // TODO acabar
-        createLocationRequest();
+        actualSubmit();
+    }
+
+    private void actualSubmit() {
+        // TODO send with api
+        Toast.makeText(this, "Submitted", Toast.LENGTH_LONG).show();
+    }
+
+    public void pickLocation(View v) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            Log.d("CreateEventActivity", "PlacePicker exception 1");
+            Toast.makeText(this, "Unable to start PlacePicker activity. Please turn on GPS and Internet connection and try again later.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Log.d("CreateEventActivity", "PlacePicker exception 2");
+            Toast.makeText(this, "Unable to start PlacePicker activity. Please turn on GPS and Internet connection and try again later.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void clearLocation(View v) {
+        event_location = null;
+        good_location_layout.setVisibility(View.GONE);
+        no_location_layout.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -157,7 +199,7 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
                     Bitmap scaled = Bitmap.createScaledBitmap(original_bitmap, 512, nh, true);
                     event_image.setImageBitmap(scaled);
 
-                    selected_image = scaled;
+                    event_selected_image = scaled;
                     select_picture_button.setText(clear_image_text);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -166,18 +208,14 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
             }
         } else if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Log.d("test", "" + place.getLatLng().latitude + " " + place.getLatLng().longitude);
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                Place place = PlacePicker.getPlace(this, data);
+                event_location = place;
+                no_location_layout.setVisibility(View.GONE);
+                good_location_layout.setVisibility(View.VISIBLE);
+                location_text.setText(place.getName());
             } else {
-                // TODO toast
+                Toast.makeText(this, "Unable to retrieve location. Please turn on GPS and Internet connection and try again.", Toast.LENGTH_LONG).show();
             }
         }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d("CreateEventActivity", "Connection Failed");
     }
 }
--- Client/DSMN-Client/dsmn/src/main/java/com/sdis/g0102/dsmn/MainActivity.java
+++ Client/DSMN-Client/dsmn/src/main/java/com/sdis/g0102/dsmn/MainActivity.java
@@ -2,11 +2,13 @@ package com.sdis.g0102.dsmn;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -17,6 +19,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 import com.sdis.g0102.dsmn.api.API;
 
 import java.io.IOException;
@@ -32,7 +37,7 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private final static int RECENT_EVENTS_CODE = 1234;
 
@@ -46,9 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
     private Button enterButton;
 
+    public static GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
@@ -62,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-        }.start();
+        };//.start();
         initEnterButton();
         initAlerts();
         initLoginButtonCallbacks();
@@ -200,6 +215,12 @@ public class MainActivity extends AppCompatActivity {
         // required by Facebook
         if(accessTokenTracker != null)
             accessTokenTracker.stopTracking();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("MainActivity", "Connection Failed");
+        Toast.makeText(this, "Unable to start Google API connection. Some functionalities may be unavailable.", Toast.LENGTH_LONG).show();
     }
 
 }
